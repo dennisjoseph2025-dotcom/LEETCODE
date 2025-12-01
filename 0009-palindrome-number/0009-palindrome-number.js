0298654ebd2d39f3e87ac0c3a7fe7a11080b270c
Dennis Joseph <dennisjoseph2025@gmail.com>
2025-12-01 15:28:24 +0530

    Time: 5 ms (78.36%), Space: 65.7 MB (11.97%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,20 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let a =x;
-   let b = [];
-   while(a>0){
-    b.push(a%10);
-    a=Math.floor(a/10);
-   } 
-   b=b.join("")
-   if(x==0){
-    return true
-   }
-   else if(b==x){
-    return true
-   }
-   else{
-    return false
-   }
+    let a = String(x).split("").reverse().join("")
+  return a===String(x)
 };

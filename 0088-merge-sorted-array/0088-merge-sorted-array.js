73d026fe20d15dda5fefd56c4bf10e2abda9303a
Dennis Joseph <dennisjoseph2025@gmail.com>
2025-12-09 10:30:20 +0530

    Time: 0 ms (100%), Space: 54.6 MB (55.68%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -10,7 +10,7 @@ var merge = function(nums1, m, nums2, n) {
     let j = n - 1;  
     let k = m + n - 1;   
 
-    while (j >= 0) {
+    for (k;j >= 0; k--) {
         if (i >= 0 && nums1[i] > nums2[j]) {
             nums1[k] = nums1[i];
             i--;
@@ -18,6 +18,5 @@ var merge = function(nums1, m, nums2, n) {
             nums1[k] = nums2[j];
             j--;
         }
-        k--;
     }
 };
